2186bba8b5617ddaa0ed097f337e347e921d486e
FlyingCatCreatures <117092071+FlyingCatCreatures@users.noreply.github.com>
2024-02-19 13:35:06 +0100

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -7,6 +7,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 import time
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction import DictVectorizer
+import joblib
 
 #Timing the program
 TrueStart=time.time()
@@ -18,12 +21,33 @@ dataset = pd.read_csv("DatsetFraud.csv") #Not included in github repository as f
 end = -1*(start-time.time())
 print("Data loaded in " + str(end) + " seconds\n")
 
-#Extracting dependent and independent variables
-x = dataset.iloc[:, [1,2,3,4,7]].values
-y = dataset.iloc[:, [10]].values
+#Separate labels
+data_labels = np.asarray(dataset.isFraud)
+
+#Encode labels
+label_encoder = LabelEncoder()
+label_encoder.fit(data_labels)
+
+
+#Apply encoding to labels
+data_labels = label_encoder.transform(data_labels)
+
+
+#Drop labels from input data
+data_selectedColums = dataset.drop(['isFraud', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest' ], axis=1)
+
+#Extract features
+start = time.time()
+print("Extracting features from dataset")
+data_features = data_selectedColums.to_dict(orient='records')
+vectorizer = DictVectorizer()
+data_features = vectorizer.fit_transform(data_features).toarray()
+end =  -1*(start-time.time())
+print("Features extracted from dataset in " + str(end) + " seconds\n")
+
 
 #Splitting dataset into training and test set
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.25, random_state=0)  
+x_train, x_test, y_train, y_test = train_test_split(data_features, data_labels, test_size= 0.99, random_state=0)  
 
 #Feature Scaling
 scaler = StandardScaler()
@@ -31,18 +55,7 @@ x_train= scaler.fit_transform(x_train)
 x_test= scaler.transform(x_test)  
 
 #Intialize Classifier and Fit KNN
-classifier= KNeighborsClassifier(
-                    n_neighbors=5,
-                    metric='minkowski',
-                    p=2
-                    )  
-
-start = time.time()
-print("Fitting KNN")
-classifier.fit(x_train, y_train)
-end =  -1*(start-time.time())
-print("KNN fit in " + str(end) + " seconds\n")
-
+classifier= joblib.load("Classifier(KNN).joblib")
 #Predicting test set result
 start = time.time()
 print("Predicting test set result")
@@ -57,3 +70,5 @@ cm = confusion_matrix(y_test, y_pred)
 print(cm)
 end =  -1*(start-time.time())
 print("Confusion Matrix created in " + str(end) + " seconds\n")
+joblib.dump(classifier, "Classifier(KNN).joblib")
+
